fa3688913eb99388ed15c499860deef326677c1f
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-04 20:04:00 +0200

    Initial commit

---
+++ src/main/java/at/bojana/impiapp/ImpiappApplication.java
@@ -0,0 +1,13 @@
+package at.bojana.impiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpiappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpiappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/bojana/impiapp/ImpiappApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bojana.impiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpiappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
